a60effd6ef63f73464db1adaeba99f464ac97f90
nikitas-k <nikitas.koussis@gmail.com>
2023-06-06 07:26:00 +1000

    [DEV] rename module

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ import numpy
 eta = Extension('neuroshape.eta', sources=['src/eta_squared.c'],
                           include_dirs=[numpy.get_include()])
 
-glmfit = Extension('neuroshape.glm', sources=['src/glmfit.c'],
+glmfit = Extension('neuroshape.stats', sources=['src/glmfit.c'],
                    include_dirs=[numpy.get_include()])
 
 # run the setup
 setup(name='neuroshape',
-      version='0.0.1',
+      version='0.0.2',
       description="For computing connectopic and geometric Laplacian eigenmodes and performing null hypothesis testing. As implementation is ongoing, this description is subject to rapid change.",
       author='Nikitas C. Koussis, Systems Neuroscience Group Newcastle',
       author_email='nikitas.koussis@gmail.com',

fcfa217f1e39107be412a9bdf316b468e7f33721
nikitas-k <nikitas.koussis@gmail.com>
2023-06-08 18:59:09 +1000

    [DEV] add eigen resampling

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ euler = Extension('neuroshape.euler', sources=['src/euler_threshold.c'],
 
 # run the setup
 setup(name='neuroshape',
-      version='0.0.4',
+      version='0.0.4.1',
       description="For computing connectopic and geometric Laplacian eigenmodes and performing null hypothesis testing. As implementation is ongoing, this description is subject to rapid change.",
       author='Nikitas C. Koussis, Systems Neuroscience Group Newcastle',
       author_email='nikitas.koussis@gmail.com',
